0bd0f4eb52a7d2cadce7c28f0bea5e64b6e8463f
Shravani-dindokar <21023080@pvgcoet.ac.in>
2025-07-20 18:58:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://dindokarshravani:Shravani$8189@cluster0.c5zntny.mongodb.net/books-collectio?retryWrites=true&w=majority&appName=Cluster0';
